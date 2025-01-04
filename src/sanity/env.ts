22bb939b32f27d03fcfc8103c3723a186ba1bea3
creativesar <uniqueluck68@gmail.com>
2025-01-04 08:19:36 +0000

    ok

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -19,17 +19,3 @@ function assertValue<T>(v: T | undefined, errorMessage: string): T {
   return v
 }
 
-
-
-// export const apiVersion =
-//   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-01-02'
-
-// export const dataset = 
-//   process.env.NEXT_PUBLIC_SANITY_DATASET  || 'production'
-  
-
-
-// export const projectId = 
-//   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "tejg9xv9"
- 
-
